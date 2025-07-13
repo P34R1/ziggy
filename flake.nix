d46904e0365fd58297c4b43f041ad51f2eca57c0
pearl <undeadgamer279@gmail.com>
2025-07-12 21:51:15 -0400

    switch to flake-utils and zig2nix

--- flake.nix
+++ flake.nix
@@ -2,34 +2,66 @@
   description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.follows = "zig2nix/flake-utils";
+    zig2nix.url = "github:Cloudef/zig2nix";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, lib, config, ... }: {
-        packages = {
-          ziggy = pkgs.stdenv.mkDerivation {
-            name = "ziggy";
-            version = "0.0.0";
-            src = ./.;
-            postPatch = ''
-              ln -s ${pkgs.callPackage ./deps.nix { }} $ZIG_GLOBAL_CACHE_DIR/p
-            '';
-            nativeBuildInputs = [ pkgs.zig.hook ];
-          };
-          default = config.packages.ziggy;
-          update-deps = pkgs.writeShellApplication {
-            name = "update-deps";
-            text = "${lib.getExe pkgs.zon2nix} > deps.nix";
-          };
+  outputs =
+    {
+      self,
+      flake-utils,
+      zig2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        env = zig2nix.outputs.zig-env.${system} { };
+      in
+      with env.pkgs.lib;
+      rec {
+        packages.foreign = env.package {
+          src = cleanSource ./.;
+          zigBuildZonLock = ./build.zig.zon2json-lock; # update with `nix run .#zon2lock`
+
+          # Packages required for compiling
+          nativeBuildInputs = with env.pkgs; [ ];
+          # Packages required for linking
+          buildInputs = with env.pkgs; [ ];
+
+          # Smaller binaries and avoids shipping glibc.
+          zigPreferMusl = true;
+          # This disables LD_LIBRARY_PATH mangling, binary patching etc...
+          # The package won't be usable inside nix.
+          zigDisableWrap = true;
         };
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ config.packages.default.nativeBuildInputs ];
+
+        packages.default = self.packages.${system}.foreign.override (attrs: {
+          # Prefer nix friendly settings.
+          zigPreferMusl = false;
+          zigDisableWrap = false;
+
+          # Executables required for runtime
+          # These packages will be added to the PATH
+          zigWrapperBins = with env.pkgs; [ ];
+
+          # Libraries required for runtime
+          # These packages will be added to the LD_LIBRARY_PATH
+          zigWrapperLibs = attrs.buildInputs or [ ];
+        });
+
+        # nix run .#zon2lock
+        apps.zon2lock = env.app [ ] "zig2nix zon2lock build.zig.zon";
+
+        devShells.default = env.mkShell {
+          # Packages required for compiling, linking and running
+          # Libraries added here will be automatically added to the LD_LIBRARY_PATH and PKG_CONFIG_PATH
+          nativeBuildInputs =
+            [ ]
+            ++ packages.default.nativeBuildInputs
+            ++ packages.default.buildInputs
+            ++ packages.default.zigWrapperBins
+            ++ packages.default.zigWrapperLibs;
         };
-      };
-    };
+      }
+    );
 }
